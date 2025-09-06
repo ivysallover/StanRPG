f5ddc1c2fd98edb92da2f352676537f7308c9ac8
ivysallover <tadeomunoz@hotmail.com.ar>
2025-09-06 12:15:44 -0300

    Add files via upload
    primer commit wacky

---
+++ batalla-naval/src/main/java/com/batallanaval/Model/Celda.java
@@ -0,0 +1,54 @@
+package com.batallanaval.Model;
+
+public class Celda {
+    private EstadoCelda estado;
+    private int barcoId; // ID del barco al que pertenece esta celda (-1 si no hay barco)
+
+    public Celda() {
+        this.estado = EstadoCelda.AGUA;
+        this.barcoId = -1;
+    }
+
+    public Celda(EstadoCelda estado) {
+        this.estado = estado;
+        this.barcoId = -1;
+    }
+
+    public Celda(EstadoCelda estado, int barcoId) {
+        this.estado = estado;
+        this.barcoId = barcoId;
+    }
+
+    // Getters y setters
+    public EstadoCelda getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoCelda estado) {
+        this.estado = estado;
+    }
+
+    public int getBarcoId() {
+        return barcoId;
+    }
+
+    public void setBarcoId(int barcoId) {
+        this.barcoId = barcoId;
+    }
+
+    public boolean tieneBarco() {
+        return barcoId != -1 && (estado == EstadoCelda.BARCO || estado == EstadoCelda.IMPACTO || estado == EstadoCelda.HUNDIDO);
+    }
+
+    public boolean fueAtacada() {
+        return estado == EstadoCelda.AGUA_ATACADA || estado == EstadoCelda.IMPACTO || estado == EstadoCelda.HUNDIDO;
+    }
+
+    @Override
+    public String toString() {
+        return "Celda{" +
+                "estado=" + estado +
+                ", barcoId=" + barcoId +
+                '}';
+    }
+}
